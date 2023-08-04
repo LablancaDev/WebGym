1853eab03a85438c8717e8a09817becedbcf72c2
LablancaDev <valenciano_david@hotmail.com>
2023-08-04 17:09:42 +0200

    commit script

--- script.js
+++ script.js
@@ -39,4 +39,37 @@ function textoUp(){
   textBottom.style.opacity = 1;
   textBottom.classList.add("efectText");
 }
-  
+
+
+//ANIMACIÓN SCROLL DESDE ABAJO IMÁGEN Y TEXTO
+let BottomPhoto = document.querySelectorAll(".imagen-main img");
+
+let BottomText = document.querySelectorAll(".texto-main");
+
+function mainScroll(){
+  let photoScroll = document.documentElement.scrollTop;
+
+  for(let i = 0; i < BottomPhoto.length; i++){
+    let longBottomPhoto = BottomPhoto[i].offsetTop;
+
+    if(longBottomPhoto -100 < photoScroll){
+      BottomPhoto[i].style.opacity = 1;
+      BottomPhoto[i].classList.add("mostrarAbajo");
+    }
+  }
+
+  let textScroll = document.documentElement.scrollTop;
+
+  for(let i = 0; i < BottomText.length; i++){
+    let longBottomText = BottomText[i].offsetTop;
+
+    if(longBottomText -100 < textScroll){
+      BottomText[i].style.opacity = 1;
+      BottomText[i].classList.add("mostrarAbajo");
+    }
+  }
+
+
+
+}
+window.addEventListener("scroll", mainScroll); 
